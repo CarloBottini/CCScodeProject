d0c8267667a5afd8573cece10900cbead49e1f96
CarloBottini <carlo.bottinimorcillo@usp.ceu.es>
2026-03-21 11:18:49 +0100

    Primer commit: Entorno y gitignore

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #This is a test
 
+import chemprop
+import torch
+
+print("=== PRUEBA DE SISTEMA ===")
+print(f"Motor PyTorch listo. Versión: {torch.__version__}")
+print(f"Cerebro Chemprop listo. Versión: {chemprop.__version__}")
+print("¡Todo está perfectamente instalado para predecir CCS!")
+
